ff814854fae629ed0a29e8ccfc7b9525354753d3
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-11-05 19:53:12 +0000

    add front form

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="grid grid-cols-1 place-content-center h-dvh place-items-center">
+    <div class="bg-neutral p-6 rounded-box w-10/12 flex flex-col gap-7">
+      <h3 class="text-3xl font-bold">Register !</h3>
+
+      <form class="flex flex-col gap-5">
+        <input type="text" placeholder="username" class="input input-bordered w-full" />
+        <input type="email" placeholder="email" class="input input-bordered w-full" />
+        <input type="password" placeholder="password" class="input input-bordered w-full" />
+        <a href="#">J'ai deja un compte</a>
+        <button class="btn btn-primary">Register</button>
+      </form>
+    </div>
   </main>
 </template>
