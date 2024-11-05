7022ce5067cff7eba45988eaa305ce2beed5b561
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-11-05 23:31:24 +0000

    new front form

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,17 +2,38 @@
 </script>
 
 <template>
-  <main class="grid grid-cols-1 place-content-center h-dvh place-items-center">
-    <div class="bg-neutral p-6 rounded-box w-10/12 flex flex-col gap-7">
-      <h3 class="text-3xl font-bold">Register !</h3>
-
-      <form class="flex flex-col gap-5">
-        <input type="text" placeholder="username" class="input input-bordered w-full" />
-        <input type="email" placeholder="email" class="input input-bordered w-full" />
-        <input type="password" placeholder="password" class="input input-bordered w-full" />
-        <a href="#">J'ai deja un compte</a>
-        <button class="btn btn-primary">Register</button>
-      </form>
+  <main class="grid grid-cols-1 place-content-center h-dvh place-items-center bg-base-200">
+    <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div class="card-body">
+        <div role="tablist" class="tabs tabs-boxed">
+          <a role="tab" class="tab tab-disabled">Login</a>
+          <a role="tab" class="tab tab-active">Register</a>
+        </div>
+        <h3 class="text-3xl font-bold my-5">Register !</h3>
+        <form>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Username</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered" required />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input type="email" placeholder="email" class="input input-bordered" required />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="password" class="input input-bordered" required />
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Register</button>
+          </div>
+        </form>
+      </div>
     </div>
   </main>
 </template>
