7495e200056d60b73a0317e3a7c43def1f35c900
swotex <swotex21@gmail.com>
2024-11-22 19:12:04 +0100

    create skeleton without slide and clean code

--- frontend/src/components/MainUser.vue
+++ frontend/src/components/MainUser.vue
@@ -1,19 +1,21 @@
 <template>
   <!-- ### Skeleton part ### -->
-  <div v-if="skeleton == true" class="carousel w-full rounded-lg static">
+  <skeleton v-if="skeleton == true">
+    <div class="carousel w-full rounded-lg static">
 
-    <div class="skeleton w-full h-full shadow-md absolute left-0 start-0 z-10">
-    </div>
+      <div class="skeleton w-full h-full shadow-md absolute left-0 start-0 z-10">
+      </div>
 
-    <div class="absolute left-0 bottom-0 w-full text-black bg-gradient-to-b from-base-100 to-base-200 flex flex-col items-start z-20">
-      <div class="p-2 w-full relative">
-        <div class="flex items-center">
-          <span class="skeleton h-10 w-2/6 text-4xl whitespace-nowrap overflow-hidden"></span>
+      <div class="absolute left-0 bottom-0 w-full bg-gradient-to-b from-base-100 to-base-200 flex flex-col items-start z-20">
+        <div class="p-2 w-full relative">
+          <div class="flex items-center">
+            <span class="skeleton h-10 w-2/6"></span>
+          </div>
+          <p class="skeleton w-full h-5 mt-1"></p>
         </div>
-        <p class="skeleton w-full h-5 text-sm mt-1"></p>
       </div>
     </div>
-  </div>
+  </skeleton>
 
   <!-- ### main part part ### -->
   <div v-if="skeleton == false" class="carousel w-full rounded-lg static">
@@ -29,11 +31,9 @@
         <img :src="image" alt="Slide image" class="w-full h-full object-cover" v-double-tap="likeUser" />
       </swiper-slide>
 
-      <!-- <div class="swiper-pagination"></div> -->
     </swiper>
     <div class="absolute left-0 bottom-0 w-full text-black bg-gradient-to-b from-transparent to-base-200  flex flex-col items-start z-20">
       <div class="p-2 w-full relative">
-        <!-- <img src="../images/double-arrow-up.svg" alt="like btn" class="absolute inset-x-[47%] mr-5 bottom-2/3 size-14 cursor-pointer" /> -->
         <img src="../images/arrow-up.svg" alt="like btn" class="absolute inset-x-[47%] mr-5 bottom-2/3 size-14 cursor-pointer" />
         <div class="flex items-center">
           <span class="text-4xl whitespace-nowrap overflow-hidden text-zinc-100">{{name}}</span>
@@ -44,7 +44,6 @@
     </div>
     <div class="absolute right-0 bottom-20 text-black m-2 z-20">
       <img :src="isLiked ? '/src/images/heart-broken.svg' : '/src/images/heart.svg'" @click="btnlike" alt="like btn" class="size-10 my-2 cursor-pointer" />
-      <!-- <img src="../images/three-dots.svg" alt="more option btn" class="size-10 my-2 cursor-pointer" /> -->
       <img src="../images/flag.svg" alt="more option btn" class="size-10 my-2 cursor-pointer" />
     </div>
     <img src="../images/heart.svg" class="absolute inset-x-1/2 inset-y-1/2 z-20 size-10 transition-transform duration-500" :class="[animated ? 'scale-[15] opacity-100' : 'scale-100 opacity-0']"/>
