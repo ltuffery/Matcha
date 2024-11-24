31ab9e20b6aff07ee1d6709fb44559f985a3fb46
swotex <swotex21@gmail.com>
2024-11-24 23:23:12 +0100

    create sender function and start to create html body of mail

--- api/src/Controllers/EmailController.php
+++ api/src/Controllers/EmailController.php
@@ -7,9 +7,50 @@ use InvalidDataException;
 use Matcha\Api\Model\User;
 use Matcha\Api\Validator\Validator;
 use ReflectionException;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function emailVerifSend($userTarget): void
+{
+    $mail = new PHPMailer(true);
+    
+    try {
+        
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'mail.delaware.o2switch.net';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'user@name.fr';                     //SMTP username
+        $mail->Password   = 'Password';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465; 
+
+        //Recipients
+        $mail->setFrom('noreply@matcha.com', 'Matcha');
+        // $mail->setFrom('bouffemoilecul@denis.fr');
+        // $mail->addAddress('swotex21@gmail.com', 'swotex');
+        $mail->addAddress($userTarget->email, $userTarget->username);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Verify your email';
+        $bodyMail = file_get_contents(__DIR__ . '/file.html');
+        $mail->Body    = $bodyMail;
+
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = 'base64';
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
 
 class EmailController
 {
+
     /**
      * @throws InvalidDataException
      * @throws ReflectionException
@@ -33,7 +74,8 @@ class EmailController
             ]);
         }
         // future check if the email is already validate or not
-        else if (true) { 
+        else if (true) {
+            emailVerifSend($user);
             Flight::json([
                 'success' => true,
             ]);
