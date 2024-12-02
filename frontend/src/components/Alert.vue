93aba36afe78ec570701bc083f9fb402fdfbf026
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-12-02 21:43:17 +0100

    error alert css class

--- frontend/src/components/Alert.vue
+++ frontend/src/components/Alert.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, ref } from 'vue';
+import { defineProps, ref } from 'vue'
 
 const props = defineProps({
   type: {
@@ -9,24 +9,30 @@ const props = defineProps({
   title: {
     type: String,
     required: true,
-  }
+  },
 })
-
-const alertClass = ref('alert-' + props.type)
 </script>
 
 <template>
-  <div role="alert" class="alert" :class="alertClass">
+  <div
+    role="alert"
+    class="alert"
+    :class="{
+      'alert-error': props.type == 'error',
+    }"
+  >
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="h-6 w-6 shrink-0 stroke-current"
       fill="none"
-      viewBox="0 0 24 24">
+      viewBox="0 0 24 24"
+    >
       <path
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="2"
-        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
     </svg>
     <div>
       <span>{{ props.title }}</span>
