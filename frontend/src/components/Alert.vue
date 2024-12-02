18afa712c44ffd5743c2ea6c81f69c01f28d59e3
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-12-02 21:54:04 +0100

    add custom svg error icon

--- frontend/src/components/Alert.vue
+++ frontend/src/components/Alert.vue
@@ -21,19 +21,7 @@ const props = defineProps({
       'alert-error': props.type == 'error',
     }"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6 shrink-0 stroke-current"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
+    <img alt="Alert icon" src="../assets/icons/close-circle-svgrepo-com.svg" class="h-8 w-8 shrink-0 stroke-current"/>
     <div>
       <span>{{ props.title }}</span>
     </div>
