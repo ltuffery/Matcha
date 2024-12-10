bebd486b1e81bc28e94c9b62e9c7a9a7b60397e4
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-12-10 11:38:21 +0000

    remove factory loop

--- api/database/seeders/UserSeeder.php
+++ api/database/seeders/UserSeeder.php
@@ -8,19 +8,17 @@ class UserSeeder implements SeederInterface
 {
     public function run(): void
     {
-        for ($i = 0; $i < 50; $i++) {
-            User::factory()->create([
-                'username' => faker()->userName,
-                'email' => faker()->email,
-                'password' => password_hash('password', PASSWORD_DEFAULT),
-                'email_verified' => true,
-                'first_name' => faker()->firstName(),
-                'last_name' => faker()->lastName,
-                'age' => rand(18, 35),
-                'gender' => array_rand(['M', 'F', 'O']) + 1,
-                'sexual_preferences' => array_rand(['M', 'F', 'O', 'A']) + 1,
-                'biography' => faker()->sentence
-            ]);
-        }
+        User::factory()->create([
+            'username' => faker()->userName,
+            'email' => faker()->email,
+            'password' => password_hash('password', PASSWORD_DEFAULT),
+            'email_verified' => true,
+            'first_name' => faker()->firstName(),
+            'last_name' => faker()->lastName,
+            'age' => rand(18, 35),
+            'gender' => array_rand(['M', 'F', 'O']) + 1,
+            'sexual_preferences' => array_rand(['M', 'F', 'O', 'A']) + 1,
+            'biography' => faker()->sentence
+        ]);
     }
 }
--- api/src/Factory/Factory.php
+++ api/src/Factory/Factory.php
@@ -7,39 +7,25 @@ use Exception;
 final class Factory
 {
     private string $model;
-    private int $count = 1;
 
     public function __construct(string $model)
     {
         $this->model = $model;
     }
 
-    public function count(int $n): static
-    {
-        if ($n <= 0) {
-            throw new Exception("The count value must be greater than zero. Provided value: {$n}");
-        }
-
-        $this->count = $n;
-
-        return $this;
-    }
-
     public function create(array $data): void
     {
-        for ($i = 0; $i < $this->count; $i++) {
-            $model = new $this->model;
+        $model = new $this->model;
 
-            try {
-                foreach ($data as $key => $value) {
-                    $model->{$key} = $value;
-                }
+        try {
+            foreach ($data as $key => $value) {
+                $model->{$key} = $value;
+            }
 
-                $model->save();
-            } catch(Exception $e) {
-                if (!getenv('PHPUNIT_TEST')) {
-                    echo sprintf("\033[0;31m%s (%d) : %s\033[0m", $this->model, $i, $e->getMessage()) . PHP_EOL;
-                }
+            $model->save();
+        } catch(Exception $e) {
+            if (!getenv('PHPUNIT_TEST')) {
+                echo sprintf("\033[0;31m[%s] \033[0m%s", $this->model, $e->getMessage()) . PHP_EOL;
             }
         }
     }
