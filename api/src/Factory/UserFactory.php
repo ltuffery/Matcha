fc536ebbccbcebb032c5815a001aa3a6dfda80ec
swotex <swotex21@gmail.com>
2024-12-19 18:09:28 +0100

    resole conversation. format date '/' to '-'

--- api/src/Factory/UserFactory.php
+++ api/src/Factory/UserFactory.php
@@ -13,7 +13,7 @@ class UserFactory extends Factory
             'email_verified' => true,
             'first_name' => faker()->firstName(),
             'last_name' => faker()->lastName,
-            'birthday' => "2000/12/12",
+            'birthday' => "2000-12-12",
             'gender' => array_rand(['M', 'F', 'O']) + 1,
             'sexual_preferences' => array_rand(['M', 'F', 'O', 'A']) + 1,
             'biography' => faker()->sentence,
