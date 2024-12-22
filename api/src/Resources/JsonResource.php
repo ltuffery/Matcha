230a46cad9da8b283c6817c3fa012d41a9f387bf
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-12-22 19:50:31 +0000

    create Resources class

---
+++ api/src/Resources/JsonResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Matcha\Api\Resources;
+
+use JsonSerializable;
+use Matcha\Api\Model\Model;
+
+abstract class JsonResource implements JsonSerializable
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param Model[] $collection
+     * @return JsonResource[]
+     */
+    public static function collection(array $collection): array
+    {
+        $resources = [];
+
+        foreach ($collection as $m) {
+            $resources[] = new self($m);
+        }
+
+        return $resources;
+    }
+}
