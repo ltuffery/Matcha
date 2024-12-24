3531d094fb3ed541835b83d8384503a5ae4114cc
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-12-24 11:42:22 +0000

    update controller

--- api/src/Controllers/ChatController.php
+++ api/src/Controllers/ChatController.php
@@ -3,6 +3,10 @@
 namespace Matcha\Api\Controllers;
 
 use Flight;
+use Matcha\Api\Model\Message;
+use Matcha\Api\Model\User;
+use Matcha\Api\Validator\Validator;
+use PDO;
 
 class ChatController
 {
@@ -15,11 +19,45 @@ class ChatController
 
     public function show(string $username): void
     {
-        // TODO: All message by sender and receiver
+        $reveiver = User::find([
+            'username' => $username,
+        ]);
+        $stmt = Flight::db()->prepare("
+            SELECT * 
+            FROM messages 
+            WHERE (sender_id = :sender_id AND receiver_id = :receiver_id)
+            OR (sender_id = :receiver_id AND receiver_id = :sender_id)
+            ORDER BY created_at;
+        ");
+
+        $stmt->execute([
+            'sender_id' => Flight::user()->id,
+            'receiver_id' => $reveiver->id,
+        ]);
+
+        $messages = array_map(
+            fn (array $data) => Message::morph($data)->getData(),
+            $stmt->fetchAll(PDO::FETCH_ASSOC)
+        );
+
+        Flight::json($messages);
     }
 
     public function store(string $username): void
     {
-        // TODO: store message
+        Validator::make([
+            'content' => 'required',
+        ]);
+
+        $reveiver = User::find([
+            'username' => $username,
+        ]);
+        $message = new Message();
+
+        $message->sender_id = Flight::user()->id;
+        $message->receiver_id = $reveiver->id;
+        $message->content = Flight::request()->data->content;
+
+        $message->save();
     }
 }
