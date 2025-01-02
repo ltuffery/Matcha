94837c10b09eb82b189036ef8420e32cbdcb4e36
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-01-03 00:48:49 +0100

    [Issue-63]: Model data rendering (#68)
    * create Resources class
    
    * fix tests fail

---
+++ api/src/Resources/SearchableUserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Matcha\Api\Resources;
+
+use Matcha\Api\Model\User;
+
+/**
+ * @property User $model
+ */
+class SearchableUserResource extends JsonResource
+{
+    public function __construct(User $user)
+    {
+        parent::__construct($user);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'username' => $this->model->username,
+            'first_name' => $this->model->first_name,
+            'last_name' => $this->model->last_name,
+            'online' => $this->model->online,
+        ];
+    }
+}
