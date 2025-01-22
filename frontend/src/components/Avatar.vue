42d371cc96a2cb62cfe261fe9668a0460f324a3c
swotex <swotex21@gmail.com>
2025-01-22 09:59:34 +0100

    Create avatar componant

---
+++ frontend/src/components/Avatar.vue
@@ -0,0 +1,61 @@
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "",
+  },
+  src: {
+    type: String,
+    require: true,
+    default: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp",
+  },
+  width: {
+    type: String,
+    default: "16",
+  },
+  addedCss: {
+    type: String,
+    default: "",
+  },
+  online: {
+    type: Boolean,
+    require: true,
+  },
+})
+</script>
+
+<template>
+
+    <template v-if="type == 'squircle'">
+        <div :class="['avatar', online ? 'online' : 'offline']">
+            <div :class="[`mask mask-squircle w-${width}`, addedCss]">
+                <img
+                    :src="src"
+                />
+            </div>
+        </div>
+
+    </template>
+
+    <template v-else-if="type == 'r-full'">
+        <div :class="['avatar', online ? 'online' : 'offline']">
+            <div :class="[`rounded-full w-${width}`, addedCss]">
+                <img
+                    :src="src"
+                />
+            </div>
+        </div>
+    </template>
+
+    <template v-else>
+        <div :class="['avatar', online ? 'online' : 'offline']">
+            <div :class="[`w-${width}`, addedCss]">
+                <img
+                    :src="src"
+                />
+            </div>
+        </div>
+    </template>
+
+</template>
