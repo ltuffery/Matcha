41e693c5282bfe88e71a1cb89c4e97833d3e8af3
swotex <swotex21@gmail.com>
2025-01-24 19:03:14 +0100

    create image card stack

---
+++ frontend/src/views/SettingsView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ProvilView from '@/components/ProfileView.vue'
+import DoubleSlide from '@/components/DoubleSlide.vue'
+
+  const temporaryImage = [
+    "https://as1.ftcdn.net/v2/jpg/06/12/64/52/1000_F_612645270_KBcBTf5CToMDyGr1hDpqSNyMK6eaXrPq.jpg",
+    "http://as1.ftcdn.net/v2/jpg/09/25/29/78/1000_F_925297895_12R4VyhRAEmvMGHEdGSXXj3B9PTHVYUa.jpg",
+    "https://as1.ftcdn.net/v2/jpg/02/44/38/08/1000_F_244380850_H3xd2rrb9CfCIcTyFcepVL670vvuTA0b.jpg"
+  ]
+</script>
+
+<template>
+	<div class="flex bg-base-300 h-dvh w-full justify-center items-center">
+		<div class="bg-base-200 h-full w-full max-w-3xl px-4">
+      <div class="flex w-full h-full flex-col">
+
+        <div class="flex self-center items-center flex-col gap-6">
+          <div class="pt-14">
+            <ProvilView class="w-20 h-36 cursor-pointer" :images="temporaryImage"/>
+          </div>
+          <div class="text-xl">First Name</div>
+        </div>
+
+        <div>Temporary</div>
+        <div class="flex">
+          <label>18</label>
+          <DoubleSlide />
+          <label>80</label>
+        </div>
+      </div>
+		</div>
+	</div>
+</template>
+<!--<div class="w-full h-full flex justify-center items-center">-->
+<!--<ProvilView class="w-20 h-36 cursor-pointer" :images="temporaryImage"/>-->
+<!--</div>-->
