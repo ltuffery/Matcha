677a12c8c60cd068f994706e8aed802312b0f610
swotex <swotex21@gmail.com>
2025-01-27 14:49:53 +0100

    clean comment

--- frontend/src/components/settings/AccountSettings.vue
+++ frontend/src/components/settings/AccountSettings.vue
@@ -155,43 +155,4 @@ const sendChangement = e => {
       <button class="btn btn-outline btn-error">Delete Account</button>
     </div>
   </div>
-
-
-
-
-
-
-
-<!--  <div>-->
-<!--    <div class="card bg-base-300 gap-3 w-full p-5">-->
-<!--        <label>Images :</label>-->
-<!--      <ImageSelector />-->
-<!--    </div>-->
-<!--  </div>-->
-
-<!--  <div>-->
-<!--    <div class="card bg-base-300 gap-3 w-full p-5">-->
-<!--      <div class="flex justify-between">-->
-<!--        <label>Bio :</label>-->
-<!--        <label>X / 512</label>-->
-<!--      </div>-->
-<!--      <textarea class="textarea textarea-bordered" style="resize: none" placeholder="Bio"></textarea>-->
-<!--    </div>-->
-<!--  </div>-->
-
-<!--  <div>-->
-<!--    <div class="card bg-base-300 gap-3 w-full p-5">-->
-<!--      <div class="flex justify-between">-->
-<!--        <label>Tags :</label>-->
-<!--        <label>X Selected</label>-->
-<!--      </div>-->
-<!--      <TagSelector class="max-h-72 overflow-y-auto" />-->
-<!--    </div>-->
-<!--  </div>-->
-
-<!--  <div>-->
-<!--    <div class="card w-full p-5 mt-10">-->
-<!--      <button class="btn btn-outline btn-error">Delete Account</button>-->
-<!--    </div>-->
-<!--  </div>-->
 </template>
