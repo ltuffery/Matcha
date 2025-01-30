1413f0a5151dcae4f1cdc3b31d2aeb398a0c8e54
swotex <swotex21@gmail.com>
2025-01-30 17:42:17 +0100

    profile page redesign

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,19 +1,74 @@
 <template>
   <div class="flex bg-base-300 h-dvh w-full justify-center items-center">
     <div class="overflow-y-auto bg-base-200 h-full w-full max-w-3xl">
-        <swiper
-          :slides-per-view="1"
-          :space-between="0"
-          :loop="true"
-          :pagination="{ clickable: true }"
-          class="w-full h-full absolute"
-        >
-          <swiper-slide v-for="(image, index) in images" :key="index">
-            <img :src="image" alt="Slide image" class="w-full h-full object-cover" />
-          </swiper-slide>
 
-          <div class="swiper-pagination"></div>
-        </swiper>
+      <div class="flex flex-col p-4 items-center gap-4 w-full h-full">
+        <div class="h-[40%] w-full ">
+          <swiper
+            :slides-per-view="1"
+            :space-between="10"
+            :loop="false"
+            :pagination="{ clickable: true }"
+            :watchOverflow="true"
+            class="w-full h-full shadow-md rounded-box z-10"
+          >
+            <swiper-slide v-for="(image, index) in images" :key="index">
+              <img
+                :src="image"
+                alt="Slide image"
+                class="w-full h-full object-cover"
+              />
+            </swiper-slide>
+          </swiper>
+        </div>
+
+        <div class="flex flex-col w-full gap-2">
+          <div class="flex justify-between">
+            <div class="flex gap-2 bg-base-300 rounded-box p-2 px-3 w-fit text-xl">
+              <div>
+                <label class="font-semibold">First Name</label>
+                <label>,</label>
+              </div>
+              <label class="">24</label>
+            </div>
+            <div class="badge badge-neutral">Woman</div>
+          </div>
+          <div class="flex gap-1 bg-base-300 rounded-box p-2 px-3 w-fit text-xl">
+            <label>At </label>
+            <label class="font-semibold">24 km</label>
+            <label>from you</label>
+          </div>
+        </div>
+
+
+        <div class="w-full">
+          <div class="flex flex-col gap-1 bg-base-300 rounded-box p-2 px-3">
+            <label class="text-base">Biography</label>
+            <div class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg">
+              <label>Biography content</label>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full">
+          <div class="flex flex-col gap-1 bg-base-300 rounded-box p-2 px-3">
+            <label class="text-base">Passions</label>
+            <div class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg">
+              <label>All Tags</label>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="flex flex-col gap-2 w-full mt-8">
+          <button class="btn btn-outline w-full">Block user</button>
+          <button class="btn btn-outline btn-error w-full">Report profile</button>
+        </div>
+
+      </div>
+
+
     </div>
   </div>
 </template>
