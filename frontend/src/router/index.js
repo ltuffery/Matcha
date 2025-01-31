be68f03a6a2a6a77da96303da1613cea43d5b43a
swotex <swotex21@gmail.com>
2025-01-31 13:03:01 +0100

    run format and uncomment authguard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,7 +10,7 @@ import SearchUsersView from '@/views/SearchUsersView.vue'
 import SettingsView from '@/views/SettingsView.vue'
 import VerifyEmailView from '@/views/VerifyEmailView.vue'
 import NewPasswordView from '@/views/NewPasswordView.vue'
-import ProfileView from "@/views/UserProfile.vue";
+import ProfileView from '@/views/UserProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -79,7 +79,7 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: ProfileView,
-      // beforeEnter: [authGuard],
+      beforeEnter: [authGuard],
     },
   ],
 })
--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="flex bg-base-300 h-dvh w-full justify-center items-center">
     <div class="overflow-y-auto bg-base-200 h-full w-full max-w-3xl">
-
       <div class="flex flex-col p-4 items-center gap-4 w-full h-full">
-        <div class="h-[40%] w-full ">
+        <div class="h-[40%] w-full">
           <swiper
             :slides-per-view="1"
             :space-between="10"
@@ -35,7 +34,9 @@
 
         <div class="flex flex-col w-full gap-2">
           <div class="flex justify-between">
-            <div class="flex gap-2 bg-base-300 rounded-box p-2 px-3 w-fit text-xl">
+            <div
+              class="flex gap-2 bg-base-300 rounded-box p-2 px-3 w-fit text-xl"
+            >
               <div>
                 <label class="font-semibold">First Name</label>
                 <label>,</label>
@@ -44,18 +45,21 @@
             </div>
             <div class="badge badge-neutral">Woman</div>
           </div>
-          <div class="flex gap-1 bg-base-300 rounded-box p-2 px-3 w-fit text-xl">
+          <div
+            class="flex gap-1 bg-base-300 rounded-box p-2 px-3 w-fit text-xl"
+          >
             <label>At </label>
             <label class="font-semibold">24 km</label>
             <label>from you</label>
           </div>
         </div>
 
-
         <div class="w-full">
           <div class="flex flex-col gap-1 bg-base-300 rounded-box p-2 px-3">
             <label class="text-base">Biography</label>
-            <div class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg">
+            <div
+              class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg"
+            >
               <label>Biography content</label>
             </div>
           </div>
@@ -64,30 +68,29 @@
         <div class="w-full">
           <div class="flex flex-col gap-1 bg-base-300 rounded-box p-2 px-3">
             <label class="text-base">Passions</label>
-            <div class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg">
+            <div
+              class="flex text-wrap overflow-y-auto bg-base-200/70 rounded-box p-2 px-3 w-full min-h-24 max-h-72 text-lg"
+            >
               <label>All Tags</label>
             </div>
           </div>
         </div>
 
-
-
         <div class="flex flex-col gap-2 w-full mt-8">
           <button class="btn btn-outline w-full">Block user</button>
-          <button class="btn btn-outline btn-error w-full">Report profile</button>
+          <button class="btn btn-outline btn-error w-full">
+            Report profile
+          </button>
         </div>
-
       </div>
-
-
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/swiper-bundle.css"; // Import Swiper styles
-import { EffectCreative } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css' // Import Swiper styles
+import { EffectCreative } from 'swiper/modules'
 
 export default {
   components: {
@@ -101,12 +104,12 @@ export default {
         'http://as1.ftcdn.net/v2/jpg/09/25/29/78/1000_F_925297895_12R4VyhRAEmvMGHEdGSXXj3B9PTHVYUa.jpg',
         'https://as1.ftcdn.net/v2/jpg/02/44/38/08/1000_F_244380850_H3xd2rrb9CfCIcTyFcepVL670vvuTA0b.jpg',
       ],
-    };
+    }
   },
   setup() {
     return {
       modules: [EffectCreative],
-    };
+    }
   },
-};
+}
 </script>
