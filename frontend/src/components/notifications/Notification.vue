34e0d59c00f16dca00a07dd1a183b7fca6d5ff3b
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-02-11 04:26:46 +0100

    add notification component

--- frontend/src/components/notifications/Notification.vue
+++ frontend/src/components/notifications/Notification.vue
@@ -3,5 +3,87 @@
 </script>
 
 <template>
+  <details class="dropdown">
+    <summary class="btn m-1">open or close</summary>
+    <div class="menu dropdown-content bg-base-100 rounded-box z-[1] max-w-96 p-2 shadow">
+      <h3 class="font-bold text-center py-2">Notification</h3>
+      <div class="divider my-0"></div>
+      <ul class="w-full overflow-y-auto">
+        <li>
+          <a class="inline-flex gap-4 w-full">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
 
+            <div class="flex flex-col w-full overflow-hidden">
+              <p class="font-semibold truncate">FirstName a fait des truc dfsjfhisudhfiusdfi dsiuhf i</p>
+              <p>At 5pm</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a class="inline-flex gap-4 w-full">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+
+            <div class="flex flex-col w-full overflow-hidden">
+              <p class="font-semibold truncate">FirstName a fait des truc dfsjfhisudhfiusdfi dsiuhf i</p>
+              <p>At 5pm</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a class="inline-flex gap-4 w-full">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+
+            <div class="flex flex-col w-full overflow-hidden">
+              <p class="font-semibold truncate">FirstName a fait des truc dfsjfhisudhfiusdfi dsiuhf i</p>
+              <p>At 5pm</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a class="inline-flex gap-4 w-full">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+
+            <div class="flex flex-col w-full overflow-hidden">
+              <p class="font-semibold truncate">FirstName a fait des truc dfsjfhisudhfiusdfi dsiuhf i</p>
+              <p>At 5pm</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a class="inline-flex gap-4 w-full">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+
+            <div class="flex flex-col w-full overflow-hidden">
+              <p class="font-semibold truncate">FirstName a fait des truc dfsjfhisudhfiusdfi dsiuhf i</p>
+              <p>At 5pm</p>
+            </div>
+          </a>
+        </li>
+
+
+      </ul>
+      <div class="divider my-0"></div>
+      <a href="#" class="text-center py-2 underline">View all</a>
+    </div>
+  </details>
 </template>
