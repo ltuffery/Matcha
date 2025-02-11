f84e42817d6f755cf4e5c48e62aae68bb498135b
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-02-11 02:37:43 +0100

    fix #128

--- websocket/src/handlers/chat.js
+++ websocket/src/handlers/chat.js
@@ -4,28 +4,28 @@ import {NOTIFICATION_TYPE} from "../enums/notification-types.js";
 
 export const sendMessage = (username, socket) => {
   return async (to, content) => {
-    if (onlineUsers.has(to)) {
-      const res = await Api.post(`/users/me/matches/${to}`)
-        .header('Authorization', 'Bearer ' + socket.handshake.auth.token)
-        .send({
+    const res = await Api.post(`/users/me/matches/${to}`)
+      .header('Authorization', 'Bearer ' + socket.handshake.auth.token)
+      .send({
         content: content
       })
 
-      if (res.status !== 201) {
-        return;
-      }
+    if (res.status !== 201) {
+      return;
+    }
 
-      const data = await res.json()
+    const data = await res.json()
 
+    if (onlineUsers.has(to)) {
       socket.to(onlineUsers.get(to).socketId).emit("receive_message", data)
       socket.to(onlineUsers.get(to).socketId).emit("notification", {
         type: NOTIFICATION_TYPE.MESSAGE,
         data: data,
       })
-
-      data.isMe = true
-
-      socket.emit("receive_message", data)
     }
+
+    data.isMe = true
+
+    socket.emit("receive_message", data)
   }
 }
