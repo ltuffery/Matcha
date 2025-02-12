998a1f1ed08bd6139e61016969de725dc2850479
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-02-12 11:09:27 +0100

    fix #143

--- api/src/Controllers/Profile/ProfileSuggestionController.php
+++ api/src/Controllers/Profile/ProfileSuggestionController.php
@@ -5,6 +5,7 @@ namespace Matcha\Api\Controllers\Profile;
 use Flight;
 use Matcha\Api\Model\User;
 use Matcha\Api\Resources\ProfileResource;
+use ReflectionException;
 
 class ProfileSuggestionController
 {
@@ -13,21 +14,10 @@ class ProfileSuggestionController
         /** @var User $user */
         $user = Flight::user();
 
-        $preferences = $user->getPreferences();
-        $sexualPreference = $preferences->sexual_preferences == 'A' ?
-            ['gender', '<', "('M', 'F', 'O')"] :
-            ['gender', '=', $preferences->sexual_preferences];
+        $users = $this->getSuggestions($user);
 
-        $users = User::where([
-            ['id', '<>', $user->id],
-            ['sexual_preferences', 'IN', "('A', '" . $user->gender . "')"],
-            $sexualPreference,
-        ]);
-        $likes = array_map(fn ($like) => $like->liked_id, $user->likes());
-
-        $users = array_filter($users, function ($value) use ($likes, $user) {
-            return !in_array($value->id, $likes)
-                && $this->inLocation($user->lat, $user->lon, $value->lat, $value->lon, 20)
+        $users = array_filter($users, function ($value) use ($user) {
+            return $this->inLocation($user->lat, $user->lon, $value->lat, $value->lon, 20)
                 && !$user->isBlocking($value);
         });
 
@@ -40,6 +30,37 @@ class ProfileSuggestionController
         Flight::json(
             ProfileResource::collection($users)
         );
+    }
+
+    /**
+     * @return User[]
+     * @throws ReflectionException
+     */
+    private function getSuggestions(User $user): array
+    {
+        $preferences = $user->getPreferences();
+
+        $sexualPreference = $preferences->sexual_preferences == 'A' ?
+            "users.gender IN ('M', 'F', 'O')" :
+            "users.gender = '$preferences->sexual_preferences'";
+
+        $stmt = Flight::db()->prepare("
+        SELECT
+            users.*
+        FROM users
+                 INNER JOIN preferences
+                            ON users.id = preferences.user_id
+                                AND preferences.sexual_preferences IN ('A', '$user->gender')
+                 INNER JOIN likes
+                            ON 1 <> likes.liked_id
+                                AND likes.user_id = users.id
+        WHERE $sexualPreference
+        GROUP BY users.username;
+        ");
+
+        $stmt->execute();
+
+        return array_map(fn($data) => User::morph($data), $stmt->fetchAll());
     }
 
     private function inLocation($lat1, $lon1, $lat2, $lon2, $rayon): bool
