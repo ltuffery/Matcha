9c8cc85bbdc4532c780313a2d7b3eb976685d355
swotex <swotex21@gmail.com>
2025-02-14 05:28:59 +0100

    create pinia store to user and page edit profile

--- frontend/src/views/EditProfileView.vue
+++ frontend/src/views/EditProfileView.vue
@@ -1,10 +1,44 @@
 <script setup>
-
 import ImageSelector from "@/components/userForm/ImageSelector.vue";
+import TagSelector from "@/components/TagSelector.vue";
+import {useUserInfoStore} from "@/store/userInfo.js";
+import {Api} from "@/utils/api.js";
+import {onMounted, ref} from "vue";
+
+const userInfoStore = useUserInfoStore()
+const info = ref({})
+
+onMounted(async () => {
+  if (!userInfoStore.user.username)
+  {
+    const res = await Api.get('/users/me').send()
+    if (res.ok)
+      userInfoStore.set(await res.json())
+  }
+  console.log(userInfoStore.user)
+  info.value.bio = userInfoStore.user.biography
+  info.value.photos = userInfoStore.user.photos
+  info.value.tags = userInfoStore.user.tags
+})
 </script>
 
 <template>
-  <div class="w-full h-96">
-    <image-selector />
+  <div>
+    <div class="w-full flex justify-center">
+      <image-selector class="w-full max-w-sm" />
+    </div>
+
+    <div class="flex flex-col gap-3 bg-base-300 rounded-box p-3">
+      <span>Biography :</span>
+      <textarea
+        v-model="info.bio"
+        class="textarea textarea-bordered min-h-28"
+        placeholder="Bio"
+      ></textarea>
+    </div>
+
+    <div class="flex flex-col gap-3 bg-base-300 rounded-box p-3">
+      <tag-selector />
+    </div>
   </div>
 </template>
