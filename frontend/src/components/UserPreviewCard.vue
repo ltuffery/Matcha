e0d24047819edcf9adf7ac4bc96bb9d19a9c6f21
swotex <swotex21@gmail.com>
2025-03-21 14:34:08 +0100

    rework user preview card

--- frontend/src/components/UserPreviewCard.vue
+++ frontend/src/components/UserPreviewCard.vue
@@ -4,86 +4,34 @@ const image_tmp =
 </script>
 
 <template>
-  <div class="flip-card">
-    <div class="flip-card-inner">
-      <div class="flip-card-front">
-        <img
-          class="absolute w-full h-full object-cover rounded-box"
-          :src="image_tmp"
-        />
-        <div class="w-full h-full flex flex-col-reverse z-20">
-          <div class="bg-base-100/40 pl-2 rounded-b-box">Username, 24</div>
-        </div>
-      </div>
-      <div class="flip-card-back"
-        :style="`background-image: url(${image_tmp});`">
-        <div class="p-1 w-full h-full flex items-start flex-col gap-1">
-          <div>Username, 24</div>
-          <div class="w-full">
-            <span class="text-sm">Biography :</span>
-            <div class="w-full">Content of Bio</div>
-          </div>
-          <div class="w-full">
-            <span class="text-sm">Tags :</span>
-            <div class="w-full">Content of tags</div>
-          </div>
-        </div>
-      </div>
+  <div class="card1">
+    <div class="card-face">
+      <div class="absolute top-0 w-full h-full rounded-box bg-black opacity-20"></div>
+      <img :src="image_tmp" class="w-full h-full object-cover rounded-box" />
+      <div class="absolute bottom-3 left-3 text-white">Name, 24</div>
+    </div>
+    <div class="card-back">
+      <button class="btn btn-xs">View profile</button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.flip-card {
-  //background-color: transparent;
-  @apply h-64 w-44 cursor-pointer;
-  //width: 190px;
-  //height: 254px;
-  perspective: 1000px;
-  //font-family: sans-serif;
+.card1 {
+  @apply relative w-48 h-64 bg-white flex justify-center rounded-box transition-all duration-[0.4s];
+  box-shadow: 0 0.625em 0.625em #00000036;
 }
 
-.flip-card-inner {
-  @apply relative w-full h-full;
-  //position: relative;
-  //width: 100%;
-  //height: 100%;
-  //text-align: center;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
+.card-face{
+  @apply w-full h-full absolute z-10 rounded-box select-none transition-all duration-[0.4s];
+  box-shadow: 0 0 0.625em #00000036;
 }
 
-.flip-card:hover .flip-card-inner {
-  transform: rotateY(180deg);
+.card-back{
+  @apply w-full h-[2.188em] absolute bottom-0 left-0 flex justify-center items-center;
 }
 
-.flip-card-front,
-.flip-card-back {
-  //box-shadow: 0 8px 14px 0 rgba(0, 0, 0, 0.2);
-  @apply absolute flex w-full h-full rounded-box border border-neutral;
-  //position: absolute;
-  //display: flex;
-  //flex-direction: column;
-  //justify-content: center;
-  //width: 100%;
-  //height: 100%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  //border: 1px solid coral;
-  //border-radius: 1rem;
-}
-
-.flip-card-front {
-  //background: linear-gradient(120deg, bisque 60%, rgb(255, 231, 222) 88%,
-  //rgb(255, 211, 195) 40%, rgba(255, 127, 80, 0.603) 48%);
-  //color: coral;
-}
-
-.flip-card-back {
-  //background: linear-gradient(120deg, rgb(255, 174, 145) 30%, coral 88%,
-  //bisque 40%, rgb(255, 185, 160) 78%);
-  //color: white;
-  @apply backdrop-blur-sm;
-  transform: rotateY(180deg);
+.card1:hover .card-face {
+  transform: translateY(-2.188em);
 }
 </style>
