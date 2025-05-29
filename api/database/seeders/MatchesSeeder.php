ffa86520c2bde7ff5d9f57ccf69bc3f3a2117e39
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-05-29 23:39:03 +0200

    fix: user like themselves && add AutoLikeException class (#178)

--- api/database/seeders/MatchesSeeder.php
+++ api/database/seeders/MatchesSeeder.php
@@ -13,6 +13,10 @@ class MatchesSeeder implements SeederInterface
         for ($i = 0; $i < count($users); $i++) {
             foreach (faker()->randomElements($users, rand(0, count($users) / 2)) as $user) {
                 try {
+                    if ($user->id === $users[$i]->id) {
+                        continue;
+                    }
+
                     $users[$i]->like($user);
                     $user->like($users[$i]);
                 } catch (\Exception) {
