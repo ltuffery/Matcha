f5718cd8ffa822da84f732117033c6ac09b39393
ltuffery <123221865+ltuffery@users.noreply.github.com>
2025-06-02 01:43:14 +0200

    Feat websocket db connection  (#177)
    * feat: init db connection in websocket server
    
    * fix: online and offline users
    
    * remove: comment code

--- websocket/src/events/index.ts
+++ websocket/src/events/index.ts
@@ -6,6 +6,7 @@ import {handleLikeEvent} from "@/events/likeEvent";
 import {handleViewEvent} from "@/events/viewEvent";
 import {handleBrowsingEvent} from "@/events/browsingEvent";
 import {cacheBrowsing} from "@/server";
+import {db} from "@/database/database";
 
 export const registerEvents = (io: Server) => {
   io.on("connection", (socket) => {
@@ -20,6 +21,16 @@ export const registerEvents = (io: Server) => {
 
     socket.emit("online_users", OnlineUsersCache.getAll()?.map(user => user.username))
 
+    OnlineUsersCache.getAll()?.forEach((user) => {
+      if (user.username === username) {
+        return
+      }
+
+      console.log(username)
+
+      socket.to(user.socketId).emit("user_online", username)
+    })
+
     // Handlers
     handleChatEvents(socket)
     handleLikeEvent(socket)
@@ -31,14 +42,18 @@ export const registerEvents = (io: Server) => {
       OnlineUsersCache.remove(username)
       cacheBrowsing.delete(username)
 
-      Api
-        .post('/users/me/offline')
-        .header('Authorization', 'Bearer ' + socket.handshake.auth.token)
-        .send()
-        .then(res => {
-          if (res.ok)
-            socket.broadcast.emit("user_offline", { username })
+      db.prepare("UPDATE users SET last_connection = ? WHERE username = ?", (err, stmt) => {
+        if (err !== null) {
+          console.log(err)
+          return
+        }
+
+        stmt.execute([new Date(), username], (err, result) => {
+          if (err == null) {
+            socket.broadcast.emit("user_offline", username)
+          }
         })
+      })
     });
   });
 };
